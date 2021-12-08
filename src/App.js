75faec9fbb9cb52a6f85670fd445560b5e27bf68
molloyconor511 <molloyconor511@gmail.com>
2021-12-08 10:51:33 +0000

    Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import BookingSection from "./sections/Booking"
-import ProductSection from "./sections/Product"
-import SignUpSection from "./sections/SignUp"
+import BookingSection from "./sections/Booking";
+import ProductSection from "./sections/Product";
+import SignUpSection from "./sections/SignUp";
+
+const signUpSection = (sign) => {
+  console.log(sign)
+}
 
 function App() {
   return (
     <div className="main-layout">
-      <SignUpSection />
+      <SignUpSection onSign={signUpSection}/>
       <ProductSection />
       <BookingSection />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/sections/SignUp/index.js
+++ src/sections/SignUp/index.js
@@ -1,12 +1,83 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function SignUpSection() {
+const SignUpSection = ({ onSign }) => {
+  const [firstName, setFirstName] = useState([]);
+  const [email, setEmail] = useState([]);
+  const [password, setPassword] = useState([]);
+  const [terms, setTerms] = useState(false);
+
+  // function handleFirstNameInput(event) {}
+  // function handleEmailInput(event) {}
+  // function handlePasswordInput(event) {}
+  // function handleTermsCheckbox(event) {}
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!firstName) {
+      alert("Enter first name before submitting");
+      return;
+    }
+
+    onSign({ firstName, email, password, terms });
+
+    setFirstName("");
+    setEmail("");
+    setPassword("");
+    setTerms(false);
+  };
+
   return (
     <section className="shadow pad-lg">
       <h2>Sign Up</h2>
-      {/* Write code here... */}
-    </section>
-  )
-}
 
-export default SignUpSection
+      <form className="form-stack" onSubmit={onSubmit}>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          //onChange={handleFirstNameInput}
+          onChange={(e) => setFirstName(e.target.value)}
+          value={firstName}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          //onChange={handleEmailInput}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          //onChange={handlePasswordInput}
+        />
+
+        <div>
+          <input
+            type="checkbox"
+            id="terms"
+            name="terms"
+            onChange={(e) => setTerms(e.currentTarget.checked)}
+            //onChange={handleTermsCheckbox}
+            checked={terms}
+          />
+          <label htmlFor="terms">I accept Terms and Conditons</label>
+        </div>
+
+        <button type="submit">Sign Up</button>
+      </form>
+    </section>
+  );
+};
+
+export default SignUpSection;
